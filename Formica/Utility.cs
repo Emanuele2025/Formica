a8b36a909c79f2000d763d692ea8b07800cd96fb
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-06 06:24:51 +0200

    Prime modifiche

---
+++ Formica/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formica
+{
+    /// <summary>
+    /// Classe per la gestione delle varie operazioni
+    /// </summary>
+    public class Utility
+    {
+    }
+}
