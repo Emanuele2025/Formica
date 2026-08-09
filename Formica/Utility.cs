fcb5f4ccc8f3e04dfdc904507fd84c8f69861dc4
Emanuele2025 <emanuelemattei@tutanota.com>
2026-08-09 09:08:28 +0200

    gestione messaggi

--- Formica/Utility.cs
+++ Formica/Utility.cs
@@ -13,7 +13,93 @@ namespace Formica
     public static class Utility
     {
 
+        #region Campi e proprietà
+
+
+
         public const string TitoloFinestra = "Formica - Programma gratuito per la gestione delle attività";
+        const string titolo = "Falco";
+        #endregion
+
+
+
+
+
+
+
+
+        #region Messaggi a video
+
+
+        /// <summary>
+        /// Messaggio informativo
+        /// </summary>
+        /// <param name="testo">testo da visualizzare</param>
+        public static void MessaggioInfo(string testo)
+        {
+            MessageBox.Show(testo, titolo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// MEssaggio di errore
+        /// </summary>
+        /// <param name="testo"> testo da visualizzare</param>
+        public static void MessaggioErrore(string testo)
+        {
+            MessageBox.Show(testo, titolo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        /// <summary>
+        /// Visualizza un messaggio per cancellare il record
+        /// </summary>
+        /// <returns>Restituisce SI se è stata scelta la volontà di cancellare record</returns>
+        public static bool CancellaRecord()
+        {
+            return MessageBox.Show("Si è certi di voler eliminare il record selezionato?", titolo, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+        /// <summary>
+        /// Messaggio di scelta si e no generico
+        /// </summary>
+        /// <param name="testo">testo da visualizzare</param>
+        /// <returns></returns>
+        public static bool MessaggioSiNo(string testo)
+        {
+            return MessageBox.Show(testo, titolo, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+
+
+
+
+
+        #endregion
+
+
+
+        #region Funzioni
+
+
+
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
